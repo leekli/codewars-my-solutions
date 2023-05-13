74cc315096a4fb7aa18a237b5b80a654b7bcdbc4
leekli <li.kirkham@live.com>
2023-05-13 10:24:47 +0100

    first kyu-7 kata

---
+++ kyu-7/validatePin.js
@@ -0,0 +1,7 @@
+// Regex validate PIN code
+// https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
+
+function validatePIN(pin) {
+  const regex = /^(\d{4}|\d{6})$/;
+  return regex.test(pin);
+}
